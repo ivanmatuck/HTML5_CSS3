3912788a3132ec9e77059a29c6ac95b6689a2b6e
Ivan Matuck <51277846+ivanmatuck@users.noreply.github.com>
2020-02-27 18:03:41 -0300

    Adicionando arquivos

---
+++ aula10_javascript.js
@@ -0,0 +1,28 @@
+function show_lua()//comentario//
+{
+	document.getElementById("lua-cheia").style.visibility="visible";
+	document.getElementById("sol").style.visibility="hidden";
+	document.getElementById("lavanda").style.visibility="hidden";
+}
+
+function show_sol()//comentario//
+{
+	document.getElementById("sol").style.visibility="visible";
+	document.getElementById("lua-cheia").style.visibility="hidden";
+	document.getElementById("lavanda").style.visibility="hidden";
+}
+
+
+function show_lavanda()//comentario//
+{
+	document.getElementById("lavanda").style.visibility="visible";
+	document.getElementById("lua-cheia").style.visibility="hidden";
+	document.getElementById("sol").style.visibility="hidden";
+}
+
+
+function someporratoda() {
+	document.getElementById("lavanda").style.visibility="hidden";
+	document.getElementById("lua-cheia").style.visibility="hidden";
+	document.getElementById("sol").style.visibility="hidden";
+}
